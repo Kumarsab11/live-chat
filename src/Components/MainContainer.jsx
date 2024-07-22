ab2f59498a0be29581f8af9a367cefeb69e9d949
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-07-22 16:11:46 +0530

    container and welcome commit

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./myStyles.css";
 import Sidebar from "./Sidebar";
-import ChatArea from "./ChatArea";
-//import { WorkArea } from "./WorkArea";
+import Welcome from "./Welcome";
+//import ChatArea from "./ChatArea";
 
 const MainContainer = () => {
+  const [conversations, setConversations] = useState([
+    {
+      name: "Teat#1",
+      lastMessage: "Last Message #1",
+      timeStamp: "today",
+    },
+    {
+      name: "Teat#2",
+      lastMessage: "Last Message #2",
+      timeStamp: "today",
+    },
+  ]);
   return (
     <div className="main-container">
       <Sidebar />
-      {/* <WorkArea /> */}
-      <ChatArea />
+      <Welcome />
+      {/* <ChatArea props={conversations[0]} /> */}
     </div>
   );
 };
