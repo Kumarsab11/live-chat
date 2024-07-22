71e56ec9d3539b65c6df66fff65075fadcbe5bf2
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-07-22 17:47:18 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import "./App.css";
-import Login from "./Components/Login";
-//import MainContainer from "./Components/MainContainer";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <MainContainer /> */}
-      <Login />
+      This is Me
     </div>
   );
 }
