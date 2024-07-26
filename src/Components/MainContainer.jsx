2e9854046966638dbb7c80b9cd77edf1f4af9895
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-07-26 14:00:52 +0530

    major commit

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-import "./myStyle.css";
+import React, { createContext, useState } from "react";
+import "./myStyles.css";
 import Sidebar from "./Sidebar";
-//import Welcome from "./Welcome";
-//import CreateGroups from "./CreateGroups";
-//import Login from "./Login";
-//import ChatArea from "./ChatArea";
-//import Users from "./Users";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const MainContainer = () => {
+export const myContext = createContext();
+function MainContainer() {
+  const dispatch = useDispatch();
+  const lightTheme = useSelector((state) => state.themeKey);
+  const [refresh, setRefresh] = useState(true);
+
   return (
-    <div className="main-container">
-      <Sidebar />
-      <Outlet />
-      {/* <ChatArea props={conversations[0]} /> */}
+    <div className={"main-container" + (lightTheme ? "" : " dark")}>
+      <myContext.Provider value={{ refresh: refresh, setRefresh: setRefresh }}>
+        <Sidebar />
+        <Outlet />
+      </myContext.Provider>
       {/* <Welcome /> */}
       {/* <CreateGroups /> */}
-      {/* <Login /> */}
+      {/* <ChatArea props={conversations[0]} /> */}
       {/* <Users /> */}
+      {/* <Groups /> */}
     </div>
   );
-};
+}
 
 export default MainContainer;
--- src/Components/MessageSelf.jsx
+++ src/Components/MessageSelf.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import "./myStyles.css";
 
-const MessageSelf = () => {
-  var props2 = { name: "You", message: "This is Sample message" };
 
+function MessageSelf({ props }) {
+  // console.log("Message self Prop : ", props);
   return (
     <div className="self-message-container">
       <div className="messageBox">
-        <p>{props2.message}</p>
-        <p className="self-timeStamp">12:00pm</p>
+        <p style={{ color: "black" }}>{props.content}</p>
+        {/* <p className="self-timeStamp" style={{ color: "black" }}>
+          12:00am
+        </p> */}
       </div>
     </div>
   );
-};
+}
 
 export default MessageSelf;
