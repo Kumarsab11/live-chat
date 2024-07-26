b1d8b908c26e9cc0d63b2116bcd8de40b1d55ba4
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-07-26 13:15:05 +0530

    Update Group.jsx

--- src/Components/Groups.jsx
+++ src/Components/Groups.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 import logo from "../Images/live-chat.png";
 import SearchIcon from "@mui/icons-material/Search";
 import "./myStyle.css";
 import { IconButton } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const Groups = () => {
+  const lightTheme = useSelector((state) => state.themeKey);
+
   return (
     <div className="list-container">
-      <div className="ug-header">
+      <div className={"ug-header" + (lightTheme ? "" : " dark")}>
         <img src={logo} style={{ height: "2rem", width: "2rem" }} alt="logo" />
-        <p className="ug-title">Available Groups</p>
+        <p className={"ug-title" + (lightTheme ? "" : " dark")}>
+          Available Groups
+        </p>
       </div>
       <div className="sb-search">
         <IconButton>
@@ -63,6 +68,4 @@ const Groups = () => {
   );
 };
 
-
-
-export default Groups
+export default Groups;
